6fa0bfd43cfc60badd21b7db0be30bb1e05f436f
atabel <atabel87@gmail.com>
2014-01-19 20:08:07 +0100

    Fixed indentation

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ var renfe = require('./renfe-scraper');
 var app = express();
 
 app.get('/zones', function(req, res){
-	res.json(renfe.zones);
+    res.json(renfe.zones);
 });
 
 app.get('/zone/:zoneId/stations', function(req, res) {
-	renfe.getStations(req.params.zoneId, function(stations) {
-		res.json(stations);
-	});
+    renfe.getStations(req.params.zoneId, function(stations) {
+        res.json(stations);
+    });
 });
 
 app.get('/zone/:zoneId/trip/from/:origin/to/:destination', function(req, res) {
-	var tripReq = {
-			zone: req.params.zoneId,
-			origin: req.params.origin,
-			destination: req.params.destination,
-			date: req.query.date
-	};
-	renfe.getTrip(tripReq, function(routes) {
-		res.json(routes);
-	});
+    var tripReq = {
+            zone: req.params.zoneId,
+            origin: req.params.origin,
+            destination: req.params.destination,
+            date: req.query.date
+    };
+    renfe.getTrip(tripReq, function(routes) {
+        res.json(routes);
+    });
 });
 
 app.listen(3000);
