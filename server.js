b475fc0d6c7b23926a87854e8efe995ebe84a376
atabel <atabel87@gmail.com>
2014-01-19 21:29:08 +0100

    Default date is today

--- server.js
+++ server.js
@@ -13,16 +13,34 @@ app.get('/zone/:zoneId/stations', function(req, res) {
     });
 });
 
+var twoDigits = function(num) {
+    return num < 10 ? '0' + num : num;
+};
+
+var today = function() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = twoDigits(now.getMonth() + 1);
+    var day = twoDigits(now.getDate());
+
+    return '' + year + month + day;
+};
+
 app.get('/zone/:zoneId/trip/from/:origin/to/:destination', function(req, res) {
     var tripReq = {
         zone: req.params.zoneId,
         origin: req.params.origin,
         destination: req.params.destination,
-        date: req.query.date
+        date: req.query.date || today()
     };
     renfe.getTrip(tripReq, function(routes) {
         res.json(routes);
     });
+});
+
+app.get('/time', function(req, res) {
+    var date = new Date();
+    res.json({today: date});
 });
 
 var port = process.env.PORT || 5000;
